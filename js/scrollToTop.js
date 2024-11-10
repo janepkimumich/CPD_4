e83ebaeab4453f57b34030c6e236ae712aacff31
Jane Kim <janepkim@umich.edu>
2024-11-10 17:52:46 -0500

    scroll to top

---
+++ js/scrollToTop.js
@@ -0,0 +1,14 @@
+// scroll to top of page
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+    }
+};
+
+scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
